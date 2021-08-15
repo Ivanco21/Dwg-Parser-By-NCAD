d6c2e1ffef26e02b790d7f5419e0582b5dd16928
Ivan <mne21z@gmail.com>
2021-08-15 22:03:13 +0700

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DwgParserByNCAD
+{   
+    public class Main
+    {
+        [CommandMethod("IndexDwgFiles")]
+        public void IndexDwgPropsToDb()
+        {
+
+
+        }
+    }
+}
